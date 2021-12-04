13536f4b6a5297f2eff45153fe3bf82221290e7e
Petrichor <yjmm10@yeah.net>
2021-12-05 03:20:37 +0800

    Update abstractOCRInfo.py

--- abstractOCRInfo.py
+++ abstractOCRInfo.py
@@ -12,7 +12,7 @@ def read_file(filepath: str, mode: Union[str, None] = "rb", encoding: Union[str,
     else:
         content = open(filepath, mode).read()
     encoding_type = chardet.detect(content)['encoding']
-    print("编码为:", encoding_type)
+    # print("编码为:", encoding_type)
     return content.decode('utf-8', errors="ignore")
 
 
@@ -23,93 +23,162 @@ def write_file(filepath: str, content: str) -> None:
     with open(filepath, 'w', encoding="utf-8") as f:
         f.write(content)
 
+def split_module(str:str):
+    pass
 
-def parse(str="""
-2021-12-03 13:32:58 [OCRFilter][Info]: OCR(DlpOCR) $version$: 1.0.0.8 on Nov 26 2021 17:25:16	, System $version$: 10.0
-2021-12-03 09:25:07 [ 7176][ 6360][         DLPOCR][Info ]: Image Path: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003dcd-0099-0000-6900-000099000000_����ͼƬ.gif.lddec
-2021-12-03 09:25:07 [ 7176][ 6360][         DLPOCR][Warn ]: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003dcd-0099-0000-6900-000099000000_����ͼƬ.gif.lddec is not a Picture!
-2021-12-03 09:25:07 [ 7176][ 6360][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 0.197ms!
-2021-12-03 09:25:07 [ 7176][ 9860][         DLPOCR][Info ]: Image Path: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003e00-0099-0000-9100-000099000000_����ͼƬ.jpg.lddec
-2021-12-03 09:25:07 [ 7176][ 9860][         DLPOCR][Debug]: Image Read Success!
-2021-12-03 09:25:07 [ 7176][ 9860][         DLPOCR][Debug]: Image resize: 567
-2021-12-03 09:25:07 [ 7176][ 9860][         DLPOCR][Debug]: ScaleParam(src_w:  667, src_h :  255, dst_w :  640, dst_h :  224,ratio_w :  0.959520,ratio_h :  0.878431)
-2021-12-03 09:25:08 [ 7176][ 9860][         DLPOCR][Info ]: Text Boxes is Empty!
-2021-12-03 09:25:08 [ 7176][ 9860][         DLPOCR][Debug]: OCR Full Time: 171.266ms
-2021-12-03 09:25:08 [ 7176][ 9860][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 171.512ms!
-2021-12-03 09:25:08 [ 7176][ 9616][         DLPOCR][Info ]: Image Path: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003e18-0099-0000-0109-100099000000_����ͼƬ.png.lddec
-2021-12-03 09:25:08 [ 7176][ 9616][         DLPOCR][Warn ]: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003e18-0099-0000-0109-100099000000_����ͼƬ.png.lddec is not a Picture!
-2021-12-03 09:25:08 [ 7176][ 9616][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 0.458ms!
-2021-12-03 09:25:08 [ 7176][ 6360][         DLPOCR][Info ]: Image Path: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003e30-0099-0000-9d00-000099000000_����ͼƬ.tif.lddec
-2021-12-03 09:25:08 [ 7176][ 6360][         DLPOCR][Warn ]: C:\DLP\Application\Repository\2021-12-03\ShareFile\SMB2_00003e30-0099-0000-9d00-000099000000_����ͼƬ.tif.lddec is not a Picture!
-2021-12-03 09:25:08 [ 7176][ 6360][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 0.392ms!
-2021-12-03 09:25:11 [ 7176][ 6360][         DLPOCR][Info ]: Image Path: C:/DLP/Application/Bak/TextExtractor/20211203092511_3/image-0023.jpg
-2021-12-03 09:25:11 [ 7176][ 6360][         DLPOCR][Debug]: Image Read Success!
-2021-12-03 09:25:11 [ 7176][ 6360][         DLPOCR][Debug]: Image resize: 553
-2021-12-03 09:25:11 [ 7176][ 6360][         DLPOCR][Debug]: ScaleParam(src_w:  653, src_h :  363, dst_w :  640, dst_h :  352,ratio_w :  0.980092,ratio_h :  0.969697)
-2021-12-03 09:25:12 [ 7176][ 6360][         DLPOCR][Info ]: Text Boxes is Empty!
-2021-12-03 09:25:12 [ 7176][ 6360][         DLPOCR][Debug]: OCR Full Time: 266.775ms
-2021-12-03 09:25:12 [ 7176][ 6360][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 267.274ms!
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Info ]: Image Path: C:/DLP/Application/Bak/TextExtractor/20211203092520_8/image-0019.jpg
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Debug]: Image Read Success!
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Debug]: Image resize: 553
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Debug]: ScaleParam(src_w:  653, src_h :  363, dst_w :  640, dst_h :  352,ratio_w :  0.980092,ratio_h :  0.969697)
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Info ]: Text Boxes is Empty!
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Debug]: OCR Full Time: 237.714ms
-2021-12-03 09:25:20 [ 7176][ 6360][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 237.874ms!
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Info ]: Image Path: C:/DLP/Application/Bak/TextExtractor/20211203092523_10/image-0023.jpg
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Debug]: Image Read Success!
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Debug]: Image resize: 553
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Debug]: ScaleParam(src_w:  653, src_h :  363, dst_w :  640, dst_h :  352,ratio_w :  0.980092,ratio_h :  0.969697)
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Info ]: Text Boxes is Empty!
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Debug]: OCR Full Time: 217.847ms
-2021-12-03 09:25:23 [ 7176][ 9616][         DLPOCR][Info ]: OCR Parse Failed, Cost Time: 217.937ms!
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Info ]: Image Path: C:/DLP/Application/Bak/TextExtractor/20211202194613_9/图片 3.png
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Image Read Success!
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Image resize: 553
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: ScaleParam(src_w:  653, src_h :  363, dst_w :  640, dst_h :  352,ratio_w :  0.980092,ratio_h :  0.969697)
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Get 2 Text Boxes, Cost 184.192ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Text Boxes to Vectors, Cost 0.007ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Merge Text Boxes, Sort Boxes and Get 2 Lines, Cost 0.017ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Vectors to Text Boxes, Cost 0.002ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Get 2 Text Boxes Image, Cost 1.305ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Don't Get Angle, doAngle: 0.
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: Text Recognition, Cost 153.350ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: OCR Full Time: 344.847ms
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Info ]: OCR Parse Success, Cost Time: 345.089ms!
-2021-12-02 19:46:13 [10160][12560][         DLPOCR][Debug]: OCR Result: 
-成功更容易光顾磨难和限辛 
-正如只有经过泥滨的道路才会留下脚印111 
+class Parse(object):
+    def __init__(self,dlpocr_dir=None) -> None:
+        # super().__init__()
+        if dlpocr_dir is None:
+            self.dlpocr_dir = "dlpocr_dir"
+        self.dlpocr_dir = dlpocr_dir
+        if not os.path.exists(self.dlpocr_dir):
+            os.makedirs(self.dlpocr_dir)
+        print(f"将日志中采集到的文本识别结果存放在{os.path.abspath(self.dlpocr_dir)}")
+        self.result_dir = "dlpocr_result"
+        if not os.path.exists(self.result_dir):
+            os.makedirs(self.result_dir)
+        print(f"将日志分析结果存放在{os.path.abspath(self.result_dir)}")
+        pass
 
-""", ocr_result_dir=None):
-    print(str)
-# OCR(DlpOCR) $version$: 1.0.0.8 on Nov 26 2021 17:25:16	, System $version$: 10.0
-    version = re.findall(
-        r"OCR\(DlpOCR\) \$version\$: (.*) on (.*)	, System \$version\$: (\d+.\d+)", str, re.S)
-    print(
-        f"最近重启的OCR组件版本：{version[0][0]}\t时间：{version[0][1]}\t系统版本：{version[0][2]}")
-    num_failed, num_success = len(list(re.finditer(
-        r"OCR Parse Failed!?", str, re.S))), len(list(re.finditer(r"OCR Parse Success?", str, re.S)))
-    print(
-        f"检测到{num_failed+num_success}个文件\t其中，识别失败：{num_failed}\t识别成功：{num_success}")
-    result = re.findall(
-        r"Image Path: (.*)\n.*?OCR Parse Success, Cost Time: (.*?)ms.*?OCR Result: \n(.*?)\n\n", str, re.S)
-    print("共获取有效数据：", len(result))
+    # 读取文件
+    def read_file(self,filepath: str, mode: Union[str, None] = "rb", encoding: Union[str, None] = None) -> str:
+        if encoding:
+            content = open(filepath, mode, encoding=encoding).read()
+        else:
+            content = open(filepath, mode).read()
+        encoding_type = chardet.detect(content)['encoding']
+        print("编码为:", encoding_type)
+        return content.decode('utf-8', errors="ignore")
 
-    # 二次提取
-    valid_info = []
-    for item in result:
-        temp = []
-        imgpath_last = re.findall(r"Image Path: (.*)\n?", item[0])[-1]
-        temp.append(os.path.splitext(os.path.basename(imgpath_last))[0])
-        temp.append(item[1])
-        temp.append(item[2])
-        valid_info.append(temp)
+    def write_file(self,filepath: str, content: str,mode="w") -> None:
+        dir = os.path.dirname(filepath)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        with open(filepath, mode, encoding="utf-8") as f:
+            f.write(content)
 
-    # 将结果输出
-    if ocr_result_dir:
-        for item in valid_info:
-            write_file(os.path.join(ocr_result_dir, item[0]+".txt"), item[2])
-    return valid_info
+    # 对整个日志进行分析,输出每次启动的有效日志（有文件输入）
+    def split_from_str(self,str_all:str)->Union[None,List]:
+        str_every_times = str_all.split("***********************************************TextExtractor_OCR.log started***********************************************")
+        if len(str_every_times)<=1:
+            print("无日志，请检查后执行")
+            return None
+
+        str_every_times = str_every_times[1:]
+        print(f"共启动{len(str_every_times)}次OCR组件")
+        
+        return str_every_times
+
+    def split_from_file(self,filepath:str)->Union[None,List]:
+        assert os.path.exists(filepath), print(os.path.abspath(filepath)+"文件不存在")
+        str_all = self.read_file(filepath=filepath)
+        return self.split_from_str(str_all=str_all)
+
+    # 分析单次日志,输出识别的文件数
+    def split_ocr_log(self,log:str,index=None)->Union[None,List]:
+        version = re.findall(r"([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*OCR\(DlpOCR\) \$version\$: (.*) on (.*)	, System \$version\$: (\d+.\d+)", log, re.S)
+        print(f"启动时间：{version[0][0]}\t\
+            组件版本：{version[0][1]}\t\
+            系统版本：{version[0][3]}")
+        # 获取每个输入
+        input_ocr_files = log.split("Image Path:")
+        if len(input_ocr_files)<=1:
+            if index is not None:
+                print(f"第{index}次启动未识别图片")
+            else:
+                print("该次启动未识别图片")
+            return None
+        input_ocr_files = input_ocr_files[1:]
+        print(f"共输入 {len(input_ocr_files)} 个文件（包含非图片）")
+
+        return input_ocr_files
+    
+    # 分析文件，分类
+    def classify_every_file(self,input_ocr_files:List)->Tuple[Union[None,List],Union[None,List],Union[None,List]]:
+        # 有效图片 
+        ocr_files_valid = [i for i in input_ocr_files if "is not a Picture" not in i]
+        if len(ocr_files_valid)==0:
+            print("输入的文件都不支持")
+        else:
+            print(f"其中支持识别的图片数: {len(ocr_files_valid)}")
+        # 求差集即可获得不支持的图片类型
+        ocr_files_invalid = list(set(input_ocr_files)-set(ocr_files_valid)) 
+        # if len(ocr_files_invalid)==0:
+        #     return None,None,None
+
+        # 识别成功
+        ocr_images = [i for i in ocr_files_valid if "OCR Parse Failed" not in i]
+        if len(ocr_images)==0:
+            print(f"支持识别的图片中全部识别失败")    
+        else:
+            print(f"支持识别的图片中识别成功图片数: {len(ocr_images)},成功率：{len(ocr_images)/len(ocr_files_valid)}")
+        # 求交集即可获得识别失败的图片 
+        ocr_images_fail = list(set(ocr_files_valid)-set(ocr_images))
+        # if len(ocr_images_fail)==0:
+        #     ocr_images_fail = None
+
+        return ocr_images,ocr_files_invalid,ocr_images_fail
+
+    # 获取单个图像的路径
+    def _get_filepath(self,ocr_image:str,filename=None)->str:
+        path = re.findall(r"^ (.*)\r\n",ocr_image)[0]
+        if filename is not None:
+            self.write_file(os.path.join(self.result_dir,filename),path+"\n","a+")
+        return path
+    def _get_time(self,ocr_image:str)->float:
+        return float(re.findall(r"OCR Parse Success, Cost Time: (.*?)ms",ocr_image)[0])
+    def _get_result(self,ocr_image:str):
+        return re.findall(r": OCR Result:? \r\n(.*)\r\n\r\n",ocr_image,re.S)[0]
+
+    def process_ocr_images(self,ocr_images:List)->Union[None,Tuple[List,List,List]]:
+        if len(ocr_images)==0:
+            return None
+        image_info = []
+        for ocr_image in ocr_images:
+            filepath = self._get_filepath(ocr_image,"ocr_success.txt")
+            time = self._get_time(ocr_image)
+            ocr_result = self._get_result(ocr_image)
+            
+            # 写入每个文件
+            filename = os.path.splitext(os.path.basename(filepath))[0]
+            self.write_file(os.path.join(self.dlpocr_dir,filename+".txt"),ocr_result)
+            image_info.append([filename,time,ocr_result])
+        return image_info
+    
+    def process_files_path(self,file_images:List,filename=None)->Union[None,List]:
+        if len(file_images)==0:
+            return None
+        filepaths = []
+        for file_image in file_images:
+            filepaths.append(self._get_filepath(file_image,filename))
+        return filepaths
+
+    def task(self,filepath)->Tuple[str,float,str]:
+        ocr_infos = []
+        # 获取每个日志
+        ocr_logs = self.split_from_file("TextExtractor_OCR2.log")
+        if ocr_logs is None:
+            return None
+        for i_log,ocr_log in enumerate(ocr_logs):
+            # 第 time_i 次启动日志
+            # 对日志划分为多个输入文件
+            input_ocr_files = self.split_ocr_log(ocr_log,i_log)
+            if input_ocr_files is None:
+                return None
+            # 对输入的文件进行分析，得到有效识别，无法识别，识别错误 列表
+            ocr_images,ocr_files_invalid,ocr_images_fail = self.classify_every_file(input_ocr_files)
+
+            ocr_files_invalid = self.process_files_path(ocr_files_invalid,"ocr_invlid.txt")
+
+            ocr_images_fail = self.process_files_path(ocr_images_fail,"ocr_fail.txt")
+
+            ocr_info =self.process_ocr_images(ocr_images)
+            ocr_infos.append(ocr_info)
+        return  ocr_infos
+
+
+
 
 
 #########################################
@@ -131,8 +200,7 @@ def get_same_file(dir1: str, dir2: str) -> List:
     dir2_list = [i for i in os.listdir(dir2)]
     same_list = list(set(dir1_list).intersection(set(dir2_list)))
 
-    print(f"{os.path.abspath(dir1)}包含文件数：{len(dir1_list)},其中无效{len(dir1_list)-len(same_list)}\
-            {os.path.abspath(dir2)}包含文件数：{len(dir2_list)},其中无效{len(dir2_list)-len(same_list)}")
+    print(f"{os.path.abspath(dir1)}包含文件数：{len(dir1_list)},其中无效{len(dir1_list)-len(same_list)}\n{os.path.abspath(dir2)}包含文件数：{len(dir2_list)},其中无效{len(dir2_list)-len(same_list)}")
 
     return same_list
 
@@ -185,25 +253,34 @@ def pparse(pre_dir, label_dir):
 
 
 def main():
-    filepath = "TextExtractor_OCR(1).log"
-    ocr_result_dir = "dlpocr_temp"
+    # filepath = "TextExtractor_OCR copy.log"
+    filepath = input("请输入日志文件名：")
+    print(filepath)
+    dlpocr_dir = "dlpocr_dir"
     # 真实值
     label_dir = "dlpocr_label"
+    result = "dlpocr_result"
 
-    str =read_file(filepath)
-    # 获取到处理后的数据,并打印文件
-    valid_info = parse(str=str,ocr_result_dir=ocr_result_dir)
+    p = Parse(dlpocr_dir=dlpocr_dir)
+    valid_infos = p.task(filepath)
 
-    data = pparse(ocr_result_dir, label_dir)
-
-    for i in valid_info:
-        if i[0]+".txt" in data.keys():
-            data[i[0]+".txt"].append(float(i[1]))
-    with open("result.csv","w",encoding="utf-8") as f:
+    result_path = os.path.join(result,"result.csv")
+    print(f"性能测试的结果文件路径：{os.path.abspath(result_path)}")
+    with open(result_path,"a+",encoding="utf-8") as f:
         f.write("路径,预测文本长度,真实文本,召回率,准确率,耗时\n")
-    with open("result.csv","a+",encoding="utf-8") as f:
-        for key in data.keys():
-            f.write(f"{key},{data[key][0]},{data[key][1]},{data[key][2]},{data[key][3]},{data[key][4]}\n")
+    for valid_info in valid_infos:
+        if valid_info is None:
+            continue 
+
+        data = pparse(dlpocr_dir, label_dir)
+
+        for i in valid_info:
+            if i[0]+".txt" in data.keys():
+                data[i[0]+".txt"].append(float(i[1]))
+ 
+        with open(result_path,"a+",encoding="utf-8") as f:
+            for key in data.keys():
+                f.write(f"{key},{data[key][0]},{data[key][1]},{data[key][2]},{data[key][3]},{data[key][4]}\n")
 
 if __name__ == '__main__':
     print("""
